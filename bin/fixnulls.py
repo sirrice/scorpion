e0e7e04b4c57e474cefa6848204b7fdca21e584a
eugene wu <sirrice@gmail.com>
2014-06-23 21:36:17 -0400

    fixnulls uses click.

--- bin/fixnulls.py
+++ bin/fixnulls.py
@@ -2,13 +2,17 @@
 # Validates that table doesn't contain any nulls
 # and optionally replaces null values with sane defaults
 #
+import click
 from sqlalchemy import create_engine
 from datetime import date
 
 
 
 def get_cols(db, tablename):
-  q = "select attname from pg_class, pg_attribute where relname = %s and attrelid = pg_class.oid and attnum > 0;"
+  q = """
+  select attname from pg_class, pg_attribute 
+  where relname = %s and attrelid = pg_class.oid and attnum > 0 and attisdropped = false;
+  """
   ret = []
   for (attr,) in db.execute(q, tablename):
     ret.append(attr)
@@ -35,36 +39,45 @@ def fix_typ(db, tablename, col, count, typ, bfix):
 
   if 'char' in typ or 'text' in typ:
     print "removing single quotes"
-    q = """UPDATE %s 
+    q = """
+    UPDATE %s 
     SET %s = replace(%s, e'\\'', '') 
-    WHERE substring(%s from e'\\'') is not null;""" % (tablename, col, col, col)
+    WHERE substring(%s from e'\\'') is not null;
+    """ % (tablename, col, col, col)
     print q
-    db.execute(q)
-    try:
-      db.commit()
-    except Exception as e:
-      print "\t%s" % e
-    print "\tsuccess"
+    if bfix:
+      db.execute(q)
+      try:
+        db.commit()
+      except Exception as e:
+        print "\t%s" % e
+      print "\tsuccess"
 
   if count == 0: return
 
   print "fixing %s as %s" % (col, typ)
   for val in vals:
-    q = "SELECT count(*) from %s where %s = %%s" % (tablename, col)
+    q = """
+    SELECT count(*) from %s where %s = %%s
+    """ % (tablename, col)
     nconflicts = db.execute(q, val).fetchone()[0]
     print "\tconflicts: %s\t%s" % (nconflicts, val)
     
-    if nconflicts is 0 and bfix:
+    if nconflicts is 0:
       print "\tupdating values to %s" % val
-      q = "UPDATE %s SET %s = %%s WHERE %s is null " % (tablename, col, col)
-      db.execute(q, val)
-      try:
-        db.commit()
-      except Exception as e:
-        print "\t%s" % e
-        pass
-      print "\tsuccess"
-      break
+      q = """
+      UPDATE %s SET %s = %%s WHERE %s is null
+      """ % (tablename, col, col)
+      print q
+      if bfix:
+        db.execute(q, val)
+        try:
+          db.commit()
+        except Exception as e:
+          print "\t%s" % e
+          pass
+        print "\tsuccess"
+        break
 
 
 
@@ -85,30 +98,44 @@ def check_and_fix(db, tablename, col, bfix):
 
   fix_typ(db, tablename, col, count, typ, bfix)
 
-import sys
-if len(sys.argv) <= 2:
-  print "python fixnulls.py <dbname> <tablename> <fix>"
-  exit()
-else:
-  dbname = sys.argv[1]
-  tablename = sys.argv[2]
-  bfix = False
-  if len(sys.argv) >= 4:
-    bfix = bool(sys.argv[3])
 
-if bfix:
-  v = raw_input("going to modify database!  enter 'x' to abort: ")
-  if 'x' in v:
-    print "aborting"
-    exit()
+def get_tables(db):
+  q = "select tablename from pg_tables where schemaname = 'public'"
+  return [str(table) for (table,) in db.execute(q).fetchall()]
 
-db = create_engine("postgresql://localhost/%s" % dbname)
 
-cols = get_cols(db, tablename)
-for col in cols:
-  check_and_fix(db, tablename, col, bfix)
 
-#from matplotlib import pyplot as plt
-#
-#plt.bar(range(len(vals)), vals)
-#plt.show()
+@click.command()
+@click.option('--bfix', is_flag=True, help="apply fixes to database.  Not setting this is same as a dryrun")
+@click.option('--noask', is_flag=True, help="skip last ditch prompt when --fix is set")
+@click.argument('dbname')
+@click.argument('tables', nargs=-1)
+def run(bfix, noask, dbname, tables):
+  if bfix and not noask:
+    v = raw_input("going to modify database!  enter 'x' to abort: ")
+    if 'x' in v:
+      print "aborting"
+      exit()
+
+
+  db = create_engine("postgresql://localhost/%s" % dbname)
+
+  if not tables:
+    tables = get_tables(db)
+
+  if not bfix:
+    print "Dry Run"
+
+  print dbname
+  print tables
+
+  print "count\ttype\tcol"
+  print "-------------------"
+  for table in tables:
+    cols = get_cols(db, table)
+    for col in cols:
+      check_and_fix(db, table, col, bfix)
+
+
+if __name__ == '__main__':
+  run()
